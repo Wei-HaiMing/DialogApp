02e66f77dcb0dce76f92e3ea0ece1190ed85d3ce
Armando Vega <152235816+Wei-HaiMing@users.noreply.github.com>
2024-10-28 04:17:18 +0000

    initial commit, a reflex app with a row header, some row information, and a dialog that opens and closes via buttons

--- DialogApp/DialogApp.py
+++ DialogApp/DialogApp.py
@@ -7,32 +7,55 @@ from rxconfig import config
 
 class State(rx.State):
     """The app state."""
-
-    ...
+    title = "Hello World"
 
 
 def index() -> rx.Component:
     # Welcome Page (Index)
     return rx.container(
-        rx.color_mode.button(position="bottom-left"),
-        rx.vstack(
-            rx.heading("Welcome to Reflex!", size="9"),
-            rx.text(
-                "Get started by editing ",
-                rx.code(f"{config.app_name}/{config.app_name}.py"),
-                size="5",
-            ),
-            rx.link(
-                rx.button("Check out our docs!"),
-                href="https://reflex.dev/docs/getting-started/introduction/",
-                is_external=True,
-            ),
-            spacing="5",
-            justify="center",
-            min_height="85vh",
-        ),
-        rx.logo(),
-    )
+        rx.flex(
+            rx.table.root(
+                rx.table.header(
+                    rx.table.row(
+                        rx.table.cell("Name"),
+                        rx.table.cell("Description"),
+                        rx.table.cell("Parent"),
+                    ), # end row
+                ), # end header
+                rx.table.body(
+                    rx.table.row(
+                        rx.table.row_header_cell("Mr. Yeet"),
+                        rx.table.cell("Bruh"),
+                        rx.table.cell(
+                            rx.vstack(
+                                rx.dialog.root(
+                                    rx.dialog.trigger(
+                                        rx.button("Open Dialog")
+                                    ),
+                                    rx.dialog.content(
+                                        rx.dialog.title(State.title),
+                                        rx.dialog.description("This is a dialog lmao"),
+                                        rx.dialog.close(
+                                            rx.button("Close Dialog", size="3"),
+                                        ),
+                                    ),
+                                    
+
+                                ),
+                                spacing="5",
+                                justify="row",
+                                min_height="85vh",
+                            ), # end vstack
+                        ),
+                    ),
+                ), # end table body
+                width="100%",
+            ), # end table root
+
+            rx.color_mode.button(position="bottom-left"),
+            
+        ), # end flex
+    ) # end container
 
 
 app = rx.App()
