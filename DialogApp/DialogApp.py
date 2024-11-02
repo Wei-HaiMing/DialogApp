9243eaa6d898f9306a34b1dff110735abc95b057
Armando Vega <152235816+Wei-HaiMing@users.noreply.github.com>
2024-11-02 02:38:20 +0000

    Added the form to the dialog that will change the values of each category, also added parent dropdown but functionality is still work in progress
    Co-authored-by: Dylan Tran <dtran.capp@gmail.com>
    Co-authored-by: Philip Pearce-Pearson <pearce_p1@denison.edu>

--- DialogApp/DialogApp.py
+++ DialogApp/DialogApp.py
@@ -9,40 +9,140 @@ from rxconfig import config
 class State(rx.State):
     """The app state."""
     title = "Hello World"
-    categories: List[Dict[str,str]] = [{"name": "Mark", "desc": "YouTuber"}, {"name": "Wade", "desc": "And Friends"}, {"name": "Bob", "desc": "And Friends"}]
+    categories: List[Dict[str, str]] = [{"name": "Mark", "desc": "YouTuber", "id": "23462364", "parent": ""}, {"name": "Wade", "desc": "And Friends", "id": "23454", "parent": ""}, {"name": "Bob", "desc": "And Friends", "id": "6532", "parent": ""}]
+    parent_categories: List[str] = ["Utilities", "Automobile", "Rent"]
+    dialog_open: bool = False
+    select_val = "Utilities"
+
+    # STILL A WORK IN PROGRESS TO ACTUALLY CHANGE THE PARENT CATEGORY DISPLAY
+    def change_select_value(self, value: str):
+        self.select_val = value
+
+    # handles the submit for the form for the name, description, and id of the category and displays the changes accordingly
+    def handle_submit(self, data: Dict[str, str]):
+        """
+        This functions works based on the premise that the key values of dictionary data are corresponding to
+        some dictionary value in self.categories. It loops through all of the categories present to find a match 
+        of the data dictionary to the self.categories[n] dictionary where n is an integer 0 <= n <= len(self.categories) - 1.
+        Once a match is found, that category index is saved and then looped through to compare with values to be changed
+        based on if the values in data does not equal to the format of empty_{val} where val is the literal string of the 
+        value of the chosen category. The purpose of this format is to ensure that unique changes are able to be made as other 
+        form submissions can apply to other categories. The change is then made. If there is no change and the submit button is 
+        simply pressed, then no change is made. 
+        """
+        index = -1 # variable to determine the index of the selected category to edit
+        
+        for i in range(len(self.categories)): # loop to actually compare changed data to categories
+            cat_name = self.categories[i]["name"] # takes the name of the category's name value
+            cat_desc = self.categories[i]["desc"] # etc. etc.
+            cat_id = self.categories[i]["id"]
+            data_name = "" 
+            data_desc = ""
+            data_id = ""
+            
+            # checking to see if the category value is in the data dictionary before assigning values
+            if cat_name in data: 
+                if data[cat_name] == "": # assigns empty values to the empty_{val} format for checking
+                    data[cat_name] = f"empty_{cat_name}"
+                    data_name = getKey(data, data[cat_name])
+            
+            # etc. etc.
+            if cat_desc in data:
+                if data[cat_desc] == "":
+                    data[cat_desc] = f"empty_{cat_desc}"
+                    data_desc = getKey(data, data[cat_desc])
+                    
+            if cat_id in data:
+                if data[cat_id] == "":
+                    data[cat_id] = f"empty_{cat_id}"
+                    data_id = getKey(data, data[cat_id])
+                    
+            # final check of each key pair value of data to compare against category
+            if self.categories[i]["name"] == data_name or self.categories[i]["desc"] == data_desc or self.categories[i]["id"] == data_id:
+                index = i
+                break
+        
+        # assigns corresponding category
+        category = self.categories[index]
+        
+        # assigns values to be changed to actual self.categories
+        for key, val in category.items():
+            if not data[val] == f"empty_{val}":
+                category[key] = data[val]
+                
+
+def getKey(my_dict: Dict[str, str], val: str) -> str:
+    # Code taken from Geeks for Geeks' way of retrieving dictionary keys
+
+    for key, value in my_dict.items():
+        if val == value:
+            return key
+
+    return "Key not found"
+
+
 
 # returns a row with the category name, description, and a dialog
 def disperseCategories(category: Dict[str, str]) -> rx.Component:
     return rx.table.row(
         rx.table.row_header_cell(category["name"]),
         rx.table.cell(category["desc"]),
-        getDialog(),
-    )
+        rx.table.cell(category["parent"]),
+        rx.vstack(
+            rx.dialog.root(
+                rx.dialog.trigger(
+                    rx.button(rx.icon("ellipsis-vertical"))
+                ),
+                rx.dialog.content(
+                    rx.dialog.title("Edit Category"),
+                    # rx.dialog.description("This is a dialog lmao"),
+                    rx.vstack(
+                        rx.form( # start of the form for each dialog, they are unique dialogs for each category
+                            rx.vstack(
+                                rx.text("Edit the category's info"),
 
-# returns a dialog with an open button, a description, and a close button
-# Dialog is formatted in a row format
-def getDialog() -> rx.Component:
-    return rx.vstack(
-        rx.dialog.root(
-            rx.dialog.trigger(
-                rx.button("Open Dialog")
-            ),
-            rx.dialog.content(
-                rx.dialog.title(State.title),
-                rx.dialog.description("This is a dialog lmao"),
-                rx.dialog.close(
-                    rx.button("Close Dialog", size="3"),
+                                rx.text("Name"),
+                                rx.input(
+                                    placeholder = category["name"],
+                                    name = category["name"],
+                                ),
+                                rx.text("Description"),
+                                rx.input(
+                                    placeholder = category["desc"],
+                                    name = category["desc"],
+                                ),
+                                rx.text("id"),
+                                rx.input(
+                                    placeholder = category["id"],
+                                    name = category["id"],
+                                ),
+
+                                rx.text("Select Parent Category"),
+                                rx.select(
+                                    State.parent_categories,
+                                    value = State.select_val,
+                                    on_change = State.change_select_value,
+                                ),
+                                rx.badge(State.select_val),
+                                rx.button("Submit", type="submit"),
+                            ),
+                            on_submit = State.handle_submit, 
+                            reset_on_submit = True,
+                        ),
+                        # rx.divider(),
+                        # rx.text(State.categories.to_string())
+                    ),
+                    rx.dialog.close(
+                        rx.button("Close Dialog", size="3"),
+                    ),
                 ),
             ),
-            
-        ),
-        spacing="5",
-        justify="row", # <- this is what makes the format into a row
-        
-        # min_height="85vh",
-    ), # end vstack
+            spacing="10",
+            justify="row", # <- this is what makes the format into a row
+            # min_height="85vh",
+        ), # end vstack
+    )
 
-# The front end of the app that displays/renders the data
 def index() -> rx.Component:
     # Welcome Page (Index)
     return rx.container(
@@ -70,3 +170,5 @@ def index() -> rx.Component:
 
 app = rx.App() # Creates intance of the app
 app.add_page(index) # Adds the index page to the app
+
+
